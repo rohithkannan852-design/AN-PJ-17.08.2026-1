69241356ca5c3bba82f70a80526eca7e54082ddf
rohithkannan852-design <rohithkannan852@gmail.com>
2026-08-24 11:37:54 +0530

    program 1 rdbms

--- solution.sql
+++ solution.sql
@@ -1,1 +1,10 @@
+create database collegeDB;
+use collegeDB;
 
+create table department (
+    DepartmentID INT(5) PRIMARY KEY,
+    DepartmentName VARCHAR(20),
+    HOD VARCHAR(20
+);
+
+describe Department;
